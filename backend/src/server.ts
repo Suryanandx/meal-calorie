30e8ae7203687e3de603ab0df30f1ff98cd6451f
Suryanand Sunil <suryanandkurup@gmail.com>
2025-07-03 23:17:32 +0530

    railway deployment checks

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import app from './app';
 import { connectDB } from './config/db';
 
-const PORT = process.env.PORT || 8000;
+const PORT = Number(process.env.PORT) || 8000;
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running at http://localhost:${PORT}`);
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Server running at http://0.0.0.0:${PORT}`);
   });
 });
